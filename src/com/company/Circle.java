949ea0989313ac3339f651cd4e93d7194b7dbe60
Nicholas <Nicholas-Por>
2018-10-02 11:25:59 -0400

    Test

---
+++ src/com/company/Circle.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Circle extends Shape
+{
+    private int radius;
+
+    public Circle( int radius)
+    {
+        super("Circle");
+        this.radius = radius;
+    }
+
+    public double getArea()
+
+
+    public double getPerimeter()
+
+
+
+
+}
